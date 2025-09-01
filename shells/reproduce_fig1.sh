b8fe99ef15334ef1862fea1e4bf85abe392909a0
junior alencar <junioralencar31415@gmail.com>
2025-09-01 18:06:05 -0300

    finish shells

--- shells/reproduce_fig1.sh
+++ shells/reproduce_fig1.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# Número de amostras a serem geradas
-x=1000  # Altere para o número desejado
-
 cd ..
 
 # Parâmetros fixos
 L=1000
-NumSamples=1000
+NumSamples=4000
 p0=1.0
 seed=-1  # suposição baseada no seu código
 type="bond"
@@ -15,10 +12,6 @@ k=1.0e-05
 NT=200
 dim=2
 
-# Loop principal
-for ((i=1; i<=x; i++)); do
-    echo "Executando amostra $i de $x..."
-    ./build/SOP $L $NumSamples $p0 $seed $type $k $NT $dim
-done
+echo "Running for size L=$L"
+./build/SOP $L $NumSamples $p0 $seed $type $k $NT $dim
 
-echo "✅ Finalizado: $x amostras geradas."
