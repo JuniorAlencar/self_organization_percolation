ea6471c52a4ac39c59d4cccff5d4583e4ef88e63
Júnior Alencar <junioralencar31415@gmail.com>
2025-11-06 15:31:58 -0300

    update figures

--- python/run_samples.py
+++ python/run_samples.py
@@ -7,6 +7,8 @@ start = 0.001
 #stop = 0.50
 n_points = 200
 
+rho = [0.125]
+
 L_lst = [128, 256, 512]
 nc_ = [2, 4, 8]
 dim = 3
@@ -26,8 +28,9 @@ for i in range(len(L_lst)):
                 if(nc == 8 and L_lst[i] == 512):
                         num_threads = 11
                 
-                stop = round(1/nc,2)
+                stop = round(1/nc, 3)
                 rho = custom_range(start, stop, n_points)
-                exec_name = f"data_{dim}D_L_{L_lst[i]}_nc_{nc}_props.sh"
+                exec_name = f"data_{dim}D_L_{L_lst[i]}_nc_{nc}_P0_{P0}_props.sh"
                 shell_data(L_lst[i], type_perc, p0, seed, k, Nt, dim,
-                        nc, num_runs[i], rho, exec_name, P0, num_threads, multi)
+                         nc, num_runs[i], rho, exec_name, P0, num_threads, multi)
+
